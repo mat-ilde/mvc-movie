c7c48099e2d2d2d94aa9cd904cb78ca0dfbe916b
mat-ilde <pascualsoto.matilde@gmail.com>
2022-08-31 11:27:06 +0100

    Added database and data

--- MvcMovie/Program.cs
+++ MvcMovie/Program.cs
@@ -1,13 +1,46 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-var builder = WebApplication.CreateBuilder(args);
+
+/*var builder = WebApplication.CreateBuilder(args);
+
+if (builder.Environment.IsDevelopment())
+{
+    builder.Services.AddDbContext<MvcMovieContext>(options =>
+        options.UseSqlite(builder.Configuration.GetConnectionString("MvcMovieContext")));
+}
+else
+{
+    builder.Services.AddDbContext<MvcMovieContext>(options =>
+        options.UseSqlServer(builder.Configuration.GetConnectionString("ProductionMvcMovieContext")));
+}*/
+
+/*var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<MvcMovieContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("MvcMovieContext") ?? throw new InvalidOperationException("Connection string 'MvcMovieContext' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+var app = builder.Build();*/
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var connectionString = builder.Configuration.GetConnectionString("MvcMovieContext")??throw new InvalidOperationException("Connection string 'MvcMovieContext' not found.");
+builder.Services.AddDbContext<MvcMovieContext>(options =>
+{
+    options.UseSqlite(connectionString);
+});
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
