62966239125d13b9ab4cfb0f15dbfd6a861f6d02
mat-ilde <pascualsoto.matilde@gmail.com>
2022-09-01 15:00:12 +0100

    Application working properly

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -6,16 +6,27 @@ namespace MvcMovie.Models
     public class Movie
     {
         public int Id { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public string Title { get; set; }
 
         [DataType(DataType.Date)]
         //Display specifies what to display for the name of a field
         [Display(Name = "Release Date")]
         public DateTime ReleaseDate { get; set; }
-        //can correctly map Price to currency in the database
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        [Required]
+        [StringLength(30)]
         public string Genre { get; set; }
+        [Range(1, 100)]
+        //can correctly map Price to currency in the database
+        [DataType(DataType.Currency)]
         [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        [StringLength(5)]
+        [Required]
         public string Rating {  get; set; }
     }
 }
