a3d8660706fc0021227570bb68f3b3c07c36c492
mat-ilde <pascualsoto.matilde@gmail.com>
2022-09-02 11:25:43 +0100

    last changes

--- MvcMovie/Views/Movies/SeeData/SeeData.cs
+++ MvcMovie/Views/Movies/SeeData/SeeData.cs
@@ -27,7 +27,6 @@ namespace MvcMovie.Models
                         ReleaseDate = DateTime.Parse("1989-2-12"),
                         Genre = "Romantic Comedy",
                         Price = 7.99M,
-                        Rating = "R"
 
                     },
 
@@ -37,8 +36,6 @@ namespace MvcMovie.Models
                         ReleaseDate = DateTime.Parse("1984-3-13"),
                         Genre = "Comedy",
                         Price = 8.99M,
-                        Rating = "R"
-
 
                     },
 
@@ -48,8 +45,6 @@ namespace MvcMovie.Models
                         ReleaseDate = DateTime.Parse("1986-2-23"),
                         Genre = "Comedy",
                         Price = 9.99M,
-                        Rating = "B"
-
                     },
 
                     new Movie
@@ -58,8 +53,6 @@ namespace MvcMovie.Models
                         ReleaseDate = DateTime.Parse("1959-4-15"),
                         Genre = "Western",
                         Price = 3.99M,
-                        Rating = "R"
-
                     }
                 );
                 context.SaveChanges();
